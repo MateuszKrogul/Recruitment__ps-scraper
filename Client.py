d63b1603f4865ec621b3d9419ce74ba23b6f0d56
piruet <mateuszkrogul@Mateuszs-MacBook-Pro.local>
2017-02-11 14:09:34 +0100

    Labels added

--- Client.py
+++ Client.py
@@ -1,6 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget
-from PyQt5.QtGui import QImage, QPalette, QBrush
-from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QLabel
+from PyQt5.QtGui import QImage, QPalette, QBrush, QFont
+from PyQt5.QtCore import QSize, Qt
 import sys
 
 class Window(QWidget):
@@ -8,12 +8,11 @@ class Window(QWidget):
     def __init__(self):
         super(Window, self).__init__()
 
-        self.initUI()
+        self.initLoginScreen()
 
-    def initUI(self):
-        self.setFixedSize(500,300)
+    def initLoginScreen(self):
 
-
+        # background image
         image = QImage("./Graphics/background.gif")
         image = image.scaled(QSize(500,300))
 
@@ -21,6 +20,23 @@ class Window(QWidget):
         palette.setBrush(10, QBrush(image))
         self.setPalette(palette)
 
+        l_username = QLabel("nr indeksu", self)
+        l_username.setFont(QFont("Arial",23))
+
+        palette = QPalette()
+        palette.setColor(QPalette.Foreground, Qt.white)
+        l_username.setPalette(palette)
+        l_username.move(120,170)
+
+        l_password = QLabel("hasło", self)
+        l_password.setFont(QFont("Arial", 23))
+        l_password.setPalette(palette)
+        l_password.move(120, 200)
+
+        font = QFont()
+        font.setFamily("")
+
+        self.setFixedSize(500, 300)
         self.setWindowTitle("Ps scraper")
         self.show()
 
