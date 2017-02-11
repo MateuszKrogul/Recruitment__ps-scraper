c6c1d4968d88c989af13a6fdbef5efed8003c169
piruet <mateuszkrogul@Mateuszs-MacBook-Pro.local>
2017-02-11 23:34:58 +0100

    On click behaviour and some of second screen added

--- Client.py
+++ Client.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QLabel, QLineEdit, QPushButton
 from PyQt5.QtGui import QImage, QPalette, QBrush, QFont, QFontMetrics, QRegion
-from PyQt5.QtCore import QSize, Qt
-import sys
+from PyQt5.QtCore import QSize, Qt, QPropertyAnimation, QRect
+import sys, time
 
 class Window(QWidget):
 
@@ -15,61 +15,118 @@ class Window(QWidget):
         # font
         font = QFont("Arial", 23)
 
-        # background image
-        image = QImage("./Graphics/background.gif")
-        image = image.scaled(QSize(500,300))
-
-        palette = QPalette()
-        palette.setBrush(10, QBrush(image))
-        self.setPalette(palette)
+        self.set_background("./Graphics/background.gif")
 
         # labels
-        l_username = QLabel("nr indeksu", self)
-        l_username.setFont(font)
+        self.l_username = QLabel("nr indeksu", self)
+        self.l_username.setFont(font)
 
         palette = QPalette()
         palette.setColor(QPalette.Foreground, Qt.white)
-        l_username.setPalette(palette)
-        l_username.move(120,170)
+        self.l_username.setPalette(palette)
+        self.l_username.move(120, 170)
 
         l_password = QLabel("hasło", self)
         l_password.setFont(font)
         l_password.setPalette(palette)
         l_password.move(120, 200)
 
+        palette.setColor(QPalette.Foreground, Qt.red)
+        self.message = QLabel("Niepoprawny login lub hasło", self)
+        self.message.move(200, 280)
+        self.message.setPalette(palette)
+        self.message.hide()
+
         font = QFont("Arial", 18)
         metrics = QFontMetrics(font)
 
         # line edits
-        le_username = QLineEdit(self)
-        le_username.setFont(QFont("Arial", 16))
-        le_username.setFixedSize(metrics.width("888888888888"), metrics.height())
-        le_username.move(260,l_username.y()+(l_username.height()/2)-(le_username.height()/2)) # center lineEdit
-        le_username.setAttribute(Qt.WA_MacShowFocusRect, 0)
-        le_username.setFocus()
+        self.le_username = QLineEdit(self)
+        self.le_username.setFont(QFont("Arial", 16))
+        self.le_username.setFixedSize(metrics.width("888888888888"), metrics.height())
+        self.le_username.move(260, self.l_username.y() + (self.l_username.height() / 2) - (self.le_username.height() / 2)) # center lineEdit
+        self.le_username.setAttribute(Qt.WA_MacShowFocusRect, 0)
+        self.le_username.setFocus()
+        self.le_username.textChanged.connect(lambda: (self.message.hide(),
+                                                      self.le_username.disconnect()))
 
-        le_password = QLineEdit(self)
-        le_password.setFont(font)
-        le_password.setFixedSize(metrics.width("888888888888"), metrics.height())
-        le_password.move(260, l_password.y() + (l_password.height()/2) - (le_password.height()/2)) # center lineEdit
-        le_password.setAttribute(Qt.WA_MacShowFocusRect,0)
-        le_password.setEchoMode(QLineEdit.Password)
+        self.le_password = QLineEdit(self)
+        self.le_password.setFont(font)
+        self.le_password.setFixedSize(metrics.width("888888888888"), metrics.height())
+        self.le_password.move(260, l_password.y() + (l_password.height() / 2) - (self.le_password.height() / 2)) # center lineEdit
+        self.le_password.setAttribute(Qt.WA_MacShowFocusRect, 0)
+        self.le_password.setEchoMode(QLineEdit.Password)
+        self.le_password.textChanged.connect(lambda : (self.message.hide(),
+                                                       self.le_password.disconnect()))
 
         # button
         submit = QPushButton(self)
-        submit.setFont(font)
-
-        submit.setStyleSheet("background-color: white")
-
+        submit.setFont(QFont("Arial", 16))
+        submit.setStyleSheet("background-color: solid")
         submit.setText("zaloguj")
-        submit.setFixedSize(le_password.width(),le_password.height()+7)
+        submit.setFixedSize(self.le_password.width(), submit.height() + 3)
         submit.move(260, 240)
+        submit.clicked.connect(self.submit_on_clicked)
 
         # window settings
         self.setFixedSize(500, 300)
         self.setWindowTitle("Ps scraper")
         self.show()
 
+    def initMenuScreen(self):
+        self.close()
+
+    def submit_on_clicked(self):
+        if False: #if wrong pass or username
+            self.message.show()
+            self.le_username.textChanged.connect(lambda: (self.message.hide(),
+                                                          self.le_username.disconnect()))
+            self.le_password.textChanged.connect(lambda: (self.message.hide(),
+                                                          self.le_password.disconnect()))
+            return
+
+        self.set_background("./Graphics/background.png")
+        for i in self.children():
+            i.deleteLater()
+        '''
+        x = 500
+        y = 600
+        self.setMaximumSize(x,y)
+        self.resize(x,y)
+        self.setFixedSize(x,y)
+        self.move(self.x(), self.y()-200)
+        '''
+
+        #TODO implement getting terms
+
+        terms = ["test1", "test2", "test3", "test4", "test5", "test6"] # get_terms
+
+        x = 5
+        y = 0
+
+        for term in terms:
+            self.temp = QPushButton(self)
+            self.temp.setFont(QFont("Arial", 16))
+            self.temp.setStyleSheet("background-color: solid")
+            self.temp.setText(term)
+            self.temp.move(x, y)
+            self.temp.show()
+            y += 30
+
+        x= self.temp.width()+10
+        self.setMaximumSize(x, y )
+        self.resize(x,y)
+        self.setFixedSize(x,y)
+
+
+    def set_background(self, path):
+        # background image
+        image = QImage(path)
+        #image = image.scaled(QSize(500, 300))
+
+        palette = QPalette()
+        palette.setBrush(10, QBrush(image))
+        self.setPalette(palette)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
